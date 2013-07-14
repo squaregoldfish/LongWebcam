ff2a94ecf166ff7298667b24d75c20889743f069
squaregoldfish <steve@squaregoldfish.co.uk>
2013-07-14 19:04:38 +0100

    Revised code structure. Ready for XML processing

--- web/longwebcam/app/controllers/upload_controller.rb
+++ web/longwebcam/app/controllers/upload_controller.rb
@@ -4,38 +4,45 @@ class UploadController < ApplicationController
     def upload
 
         response_code = :ok
+        upload_data = nil
 
         # Only POSTs are allowed
         if !request.post?
             response_code = :forbidden
-        else
-            data_present = true
-            upload_data = nil
+        end
 
-            # Make sure the required parameter has been provided
+        # Make sure the required parameter has been provided
+        if response_code == :ok
             if !params.key?(:image_details)
                 response_code = :bad_request
-            else
-                upload_data = params.fetch(:image_details)
+            end
+        end
 
-                # Validate the uploaded XML against the schema
-                xml = LibXML::XML::Parser.string(upload_data).parse
-                schema = LibXML::XML::Schema.new("#{RAILS_ROOT}/resources/xml/image_upload.xsd")
+        if response_code == :ok
+            # Read in the XML and validate it against the schema
+            upload_data = params.fetch(:image_details)
+            xml = LibXML::XML::Parser.string(upload_data).parse
+            schema = LibXML::XML::Schema.new("#{RAILS_ROOT}/resources/xml/image_upload.xsd")
 
-                schema_valid = true
-                begin
-                    validation_result = xml.validate_schema(schema)
-                rescue
-                    schema_valid = false
-                end
-
-
-                @message = schema_valid
+            schema_valid = true
+            begin
+                validation_result = xml.validate_schema(schema)
+            rescue
+                schema_valid = false
             end
 
-            if response_code != :ok
-                head response_code
+            if !schema_valid
+                response_code = :bad_request
             end
+        end
+
+        # At this point, we know the request itself is valid (at the HTTP level at least).
+        # Now we pull apart the actual XML and process it.
+
+        @message = "Happy"
+
+        if response_code != :ok
+            head response_code
         end
     end
 end
