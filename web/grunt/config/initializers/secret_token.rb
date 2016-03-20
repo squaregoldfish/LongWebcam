a0518d358474751b636d4659e2eb52ac03474589
squaregoldfish <steve@squaregoldfish.co.uk>
2016-03-20 20:44:29 +0000

    Base grunt application

---
+++ web/grunt/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Grunt::Application.config.secret_key_base = 'd3cd6ac485320565d7f7e93d938de18eb920fc866cac3802800a7a7d613127c9cd7b8b45a2ce64ab0d36242c074f51aaab2203a9d541552368f6dac1b1df1b93'
---
+++ web/grunt/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Grunt::Application.config.session_store :cookie_store, key: '_grunt_session'
