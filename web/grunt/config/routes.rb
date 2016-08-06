fb5212a8ced880d6d3fbd6b3374bf8847ad7f426
squaregoldfish <steve@squaregoldfish.co.uk>
2016-08-06 20:11:01 +0200

    Default index page

--- web/grunt/config/routes.rb
+++ web/grunt/config/routes.rb
@@ -1,4 +1,9 @@
 Grunt::Application.routes.draw do
+
+  # Root
+  root :to => 'welcome#index'
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
