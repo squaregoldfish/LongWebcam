3b565284e1365c6f29079785c8f954a8cc357e1a
squaregoldfish <steve@squaregoldfish.co.uk>
2017-05-20 18:07:28 +0200

    We have a map.

---
+++ web/longwebcam/app/assets/javascripts/search.js
@@ -0,0 +1,27 @@
+// Page load JS. Decides what to do based on the page contents
+$(document).ready(function() {
+  if ($('#searchMap').length) {
+    drawSearchMap();
+  }
+});
+
+function drawSearchMap() {
+	mapSource = new ol.source.Stamen({
+  		layer: "terrain",
+  		url: "https://stamen-tiles-{a-d}.a.ssl.fastly.net/terrain/{z}/{x}/{y}.png"
+	});
+
+	map = new ol.Map({
+ 		target: 'searchMap',
+ 		layers: [
+    		new ol.layer.Tile({
+        		source: mapSource
+    		})
+  		],
+  		view: new ol.View({
+    		center: ol.proj.fromLonLat([10, 45]),
+    		zoom: 4,
+    		minZoom: 2
+  		})
+	});
+}
--- web/longwebcam/config/initializers/assets.rb
+++ web/longwebcam/config/initializers/assets.rb
@@ -1,5 +1,5 @@
 # From http://theflyingdeveloper.com/controller-specific-assets-with-rails-4/
 # (Also in Evernote)
-%w( alpha ).each do |controller|
+%w( alpha search ).each do |controller|
   Rails.application.config.assets.precompile += ["#{controller}.js", "#{controller}.css"]
 end
