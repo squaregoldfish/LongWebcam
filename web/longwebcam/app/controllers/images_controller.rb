8af53407a941233c967db6405f66672a2c4b3e4e
squaregoldfish <steve@squaregoldfish.co.uk>
2017-06-05 20:05:33 +0200

    Generate thumbnail if missing

--- web/longwebcam/app/controllers/images_controller.rb
+++ web/longwebcam/app/controllers/images_controller.rb
@@ -1,3 +1,5 @@
+require 'rmagick'
+
 class ImagesController < ApplicationController
 	include ImagesHelper
 	include PrivilegesHelper
@@ -11,8 +13,21 @@ class ImagesController < ApplicationController
 			if camera.test_camera? && !can_view_test_cameras
 				head 403
 			else
-				file_location = get_image_location(image.camera_id, image.date) 
-				send_file(file_location, disposition: "inline")
+				thumbnail_location = get_thumbnail_location(image.camera_id, image.date)
+				
+				if !File.exist?(thumbnail_location)
+					full_image_location = get_image_location(image.camera_id, image.date)
+				
+					if !File.exists?(full_image_location)
+						head 404
+					else
+						image = Magick::Image.read(full_image_location).first
+						thumbnail = image.resize_to_fit(300, 300)
+						thumbnail.write(thumbnail_location) { self.quality = 85 }
+					end
+				end
+
+				send_file(thumbnail_location, disposition: "inline")
 			end
 		rescue ActiveRecord::RecordNotFound
 			head 404
--- web/longwebcam/app/helpers/images_helper.rb
+++ web/longwebcam/app/helpers/images_helper.rb
@@ -4,4 +4,8 @@ module ImagesHelper
 		return "#{APP_CONFIG["image_store"]}/#{camera_id}/#{date}.png"
 	end
 
+	def get_thumbnail_location(camera_id, date)
+		return "#{APP_CONFIG["image_store"]}/#{camera_id}/#{date}_thumb.jpg"
+	end
+
 end
